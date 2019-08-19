548018b6312a0017e1901eb765d81a48b3f9e77e
michael.haberzettel <michael.haberzettel@vinci-autoroutes.com>
2019-08-19 17:30:01 +0200

    Réécriture simplifiée des tests.

--- src/reducers/index.unit.test.ts
+++ src/reducers/index.unit.test.ts
@@ -3,34 +3,51 @@ import ReactDOM from 'react-dom';
 import rootReducer, { IStoreData } from '.';
 import { addArticleInBasket, removeArticleInBasket } from '../actions';
 
+function addArticlesInState(currentState: IStoreData, articleInfo: { id: string; nb: number }) {
+    const addArticlesPayload = { idArticle: articleInfo.id, nb: articleInfo.nb, unitPrice: 0.1 };
+    const addArticlesAction = addArticleInBasket(addArticlesPayload);
+    return rootReducer(currentState, addArticlesAction);
+}
+
+function removeArticleInState(currentState: IStoreData, articleInfo: { id: string; nb: number }) {
+    const removeArticlesPayload = { idArticle: articleInfo.id, nb: articleInfo.nb };
+    const removeArticlesAction = removeArticleInBasket(removeArticlesPayload);
+    return rootReducer(currentState, removeArticlesAction);
+}
+
+function createStoreWithArticle(articleInfo: { idArticle: string; nb: number }) : IStoreData {
+    const store :IStoreData = { basket:{} };
+    store.basket[articleInfo.idArticle] = { idArticle : articleInfo.idArticle, nb:articleInfo.nb,  unitPrice: 0.1 }
+    return store;
+}
 
 describe('shopping reducer', () => {
-    const add2ArticlesPayload = { idArticle: "5", nb: 3, unitPrice: 0.1 };
-    const add2ArticlesAction = addArticleInBasket(add2ArticlesPayload);
-    const reducerWith3Articles = rootReducer({ basket: {} }, add2ArticlesAction);
 
-    it('should add 3 articles in empty reducer', () => {
-        const expectedData: IStoreData = {
-            basket: { "5": { idArticle: "5", nb: 3, unitPrice: 0.1 } }
-        }
-        expect(reducerWith3Articles).toMatchObject(expectedData);
+    const stateWith3Articles = addArticlesInState({ basket: {} }, { id: '5', nb: 3 });
+    it('should add 3 articles in empty state', () => {
+        const expectedData = createStoreWithArticle({ idArticle: "5", nb: 3});
+        expect(stateWith3Articles).toMatchObject(expectedData);
+    });
+ 
+    const stateWith5Articles = addArticlesInState(stateWith3Articles, { id: "5", nb: 2});
+    it('should add 2 more articles in existing article', () => {
+        const expectedData = createStoreWithArticle({ idArticle: "5", nb: 5});
+        expect(stateWith5Articles).toMatchObject(expectedData);
     })
 
-    const remove1ArticleAction = removeArticleInBasket({ idArticle: "5", nb: 1 });
-    const reducerWith2Articles = rootReducer(reducerWith3Articles, remove1ArticleAction);
+    const stateWith2Articles = removeArticleInState(stateWith5Articles, { id: "5", nb: 3 });
     it('should decrement articles on remaining', () => {
-        const expectedData: IStoreData = {
-            basket: { "5": { idArticle: "5", nb: 2, unitPrice: 0.1 } }
-        }
-        expect(reducerWith2Articles).toMatchObject(expectedData);
+        const expectedData = createStoreWithArticle({ idArticle: "5", nb: 2});
+        expect(stateWith2Articles).toMatchObject(expectedData);
     })
 
-    const remove2ArticleAction = removeArticleInBasket({ idArticle: "5", nb: 2 });
-    const reducerWith0Article = rootReducer(reducerWith2Articles, remove2ArticleAction);
+    const stateWith0Article = removeArticleInState(stateWith2Articles, { id: "5", nb: 2 });
     it('should delete articles on leftover', () => {
-        const expectedData: IStoreData = {
-            basket: {  }
-        }
-        expect(reducerWith0Article).toMatchObject(expectedData);
+        expect(Object.values(stateWith0Article.basket).length).toEqual(0);
+    })
+
+    const stateWith0ArticleBis = removeArticleInState(stateWith0Article, { id: "5", nb: 2 });
+    it('should do nothing when remove unkwown article', () => {
+        expect(Object.values(stateWith0ArticleBis.basket).length).toEqual(0);
     })
 })
